7cb0fc988e738ae55e74bc421f0cfbb59bbce1d2
Akash Singh <akashsingh2210670@gmail.com>
2024-04-25 18:00:50 +0530

    Added Clap for cli commands and args
    Signed-off-by: Akash Singh <akashsingh2210670@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
 use reqwest::Error;
+use clap::{Parser, Subcommand};
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    cmd: Commands
+}
 
-
+#[derive(Subcommand, Debug, Clone)]
+enum Commands {
+    #[command(about = "List all gists")]
+    List,
+    #[command(about = "Create a new gist")]
+    Create,
+    #[command(about = "Edit a gist")]
+    Edit,
+    #[command(about = "Delete a gist")]
+    Delete,
+    #[command(about = "View a gist")]
+    View,
+    #[command(about = "Star a gist")]
+    Star,
+    #[command(about = "Unstar a gist")]
+    Unstar,
+    #[command(about = "List all stargazers of a gist")]
+    Stargazers,
+}
 
 use gist_magic_lib::requests; 
 
@@ -18,7 +43,34 @@ use gist_magic_lib::requests;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    
+  let args = Args::parse();
+  
+  match args.cmd{
+      Commands::List => {
+        println!("List all gists");
+      },
+      Commands::Create => {
+          println!("Create a new gist");
+      },
+      Commands::Edit => {
+          println!("Edit a gist");
+      },
+      Commands::Delete => {
+          println!("Delete a gist");
+      },
+      Commands::View => {
+          println!("View a gist");
+      },
+      Commands::Star => {
+          println!("Star a gist");
+      },
+      Commands::Unstar => {
+          println!("Unstar a gist");
+      },
+      Commands::Stargazers => {
+          println!("List all stargazers of a gist");
+      }
+  }
     let request_url: &str = "https://api.github.com/gists";
 
 
